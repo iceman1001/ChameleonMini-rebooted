7cf67e227bbe6e10738d255363a6d78b08d33267
iceman1001 <iceman@iuse.se>
2017-11-28 10:18:47 +0100

    chg:  Ultralight fixes from RevG/ and latest RevE.

--- Firmware/Chameleon-Mini/Application/MifareUltralight.c
+++ Firmware/Chameleon-Mini/Application/MifareUltralight.c
@@ -80,84 +80,90 @@ void MifareUltralightAppTask(void)
 
 uint16_t MifareUltralightAppProcess(uint8_t* Buffer, uint16_t BitCount)
 {
+    uint8_t Cmd = Buffer[0];
 
-    //memcpy(dataaa,Buffer,BitCount);
-	//datalog=BitCount;
-	if (State==STATE_COMPAT_WRITE)
-	{
-        /* Compatibility write. Receiving 16 bytes of data of which 4 bytes are valid. */
-        if (ISO14443ACheckCRCA(Buffer, BYTES_PER_COMPAT_WRITE)) 
-		{
-            /* We don't perform any checks here. You will be able to program the
-            * whole memory. Also there is no OTP behaviour. */
-            if (!ActiveConfiguration.ReadOnly) {
-                MemoryWriteBlock(Buffer, CompatWritePageAddress * BYTES_PER_PAGE, BYTES_PER_WRITE);
-            } else {
-                /* If we are told to be read only, we silently ignore the write command
-                * and pretend to have written data. */
+    switch(State) {
+    case STATE_IDLE:
+    case STATE_HALT:
+        if (ISO14443AWakeUp(Buffer, &BitCount, ATQA_VALUE)) {
+            /* We received a REQA or WUPA command, so wake up. */
+            State = STATE_READY1;
+            return BitCount;
+        }
+        break;
+
+    case STATE_READY1:
+        if (ISO14443AWakeUp(Buffer, &BitCount, ATQA_VALUE)) {
+            State = STATE_READY1;
+            return BitCount;
+        } else if (Cmd == ISO14443A_CMD_SELECT_CL1) {
+            /* Load UID CL1 and perform anticollision. Since
+            * MF Ultralight use a double-sized UID, the first byte
+            * of CL1 has to be the cascade-tag byte. */
+            uint8_t UidCL1[ISO14443A_CL_UID_SIZE] = { [0] = ISO14443A_UID0_CT };
+
+            MemoryReadBlock(&UidCL1[1], UID_CL1_ADDRESS, UID_CL1_SIZE);
+
+            if (ISO14443ASelect(Buffer, &BitCount, UidCL1, SAK_CL1_VALUE)) {
+                /* CL1 stage has ended successfully */
+                State = STATE_READY2;
             }
 
-            State = STATE_ACTIVE;
-            Buffer[0] = ACK_VALUE;
-            return ACK_FRAME_SIZE;
-        } 
-		else 
-		{
-            State = STATE_ACTIVE;
-            Buffer[0] = NAK_CRC_ERROR;
-            return NAK_FRAME_SIZE;
+            return BitCount;
+        } else {
+            /* Unknown command. Enter halt state */
+            State = STATE_IDLE;
         }
-	}
-else
-{
+        break;
 
+    case STATE_READY2:
+        if (ISO14443AWakeUp(Buffer, &BitCount, ATQA_VALUE)) {
+            State = STATE_READY1;
+            return BitCount;
+        } else if (Cmd == ISO14443A_CMD_SELECT_CL2) {
+            /* Load UID CL2 and perform anticollision */
+            uint8_t UidCL2[ISO14443A_CL_UID_SIZE];
 
-	if (ISO14443AWakeUp(Buffer, &BitCount, ATQA_VALUE))
-            return BitCount;
-	//发来0x93 0x20 或 0x93 0x70
-	else if (Buffer[0] == ISO14443A_CMD_SELECT_CL1)
-	{
-            uint8_t UidCL1[ISO14443A_CL_UID_SIZE] = { [0] = ISO14443A_UID0_CT };
-            MemoryReadBlock(&UidCL1[1], UID_CL1_ADDRESS, UID_CL1_SIZE);
-		if (ISO14443ASelect(Buffer, &BitCount, UidCL1, SAK_CL1_VALUE))
-            return BitCount;
-	}
-	//发来0x95 0x20 或 0x95 0x70
-	else if (Buffer[0] == ISO14443A_CMD_SELECT_CL2)
-	{
-		uint8_t UidCL2[4];
             MemoryReadBlock(UidCL2, UID_CL2_ADDRESS, UID_CL2_SIZE);
-		if (ISO14443ASelect(Buffer, &BitCount, UidCL2, SAK_CL2_VALUE))
+
+            if (ISO14443ASelect(Buffer, &BitCount, UidCL2, SAK_CL2_VALUE)) {
+                /* CL2 stage has ended successfully. This means
+                * our complete UID has been sent to the reader. */
+                State = STATE_ACTIVE;
+            }
+
             return BitCount;
-	}
-	else if (Buffer[0] == CMD_READ)
-	{
+        } else {
+            /* Unknown command. Enter halt state */
+            State = STATE_IDLE;
+        }
+        break;
+
+    case STATE_ACTIVE:
+        if (ISO14443AWakeUp(Buffer, &BitCount, ATQA_VALUE)) {
+            State = STATE_READY1;
+            return BitCount;
+        } else if (Cmd == CMD_READ) {
             uint8_t PageAddress = Buffer[1];
-	if (ISO14443ACheckCRCA(Buffer, CMD_READ_FRAME_SIZE)) 
-	{
+
+            if (ISO14443ACheckCRCA(Buffer, CMD_READ_FRAME_SIZE)) {
                 if (   (PageAddress >= PAGE_READ_MIN)
-            && (PageAddress <= PAGE_READ_MAX) ) 
-			{
+                    && (PageAddress <= PAGE_READ_MAX) ) {
                     /* TODO: Missing address wrap around behaviour.
                     * Implement using a for-loop copying 4 bytes each iteration
                     * and mask pageaddress */
                     MemoryReadBlock(Buffer, PageAddress * BYTES_PER_PAGE, BYTES_PER_READ);
                     ISO14443AAppendCRCA(Buffer, BYTES_PER_READ);
                     return (BYTES_PER_READ + ISO14443A_CRCA_SIZE) * 8;
-			} 
-			else
-			{
+                } else {
                     Buffer[0] = NAK_INVALID_ARG;
                     return NAK_FRAME_SIZE;
                 }
-	 } 
-	else 
-	{
+            } else {
                 Buffer[0] = NAK_CRC_ERROR;
                 return NAK_FRAME_SIZE;
             }
-    }
-   else if (Buffer[0]  == CMD_WRITE) {
+        } else if (Cmd == CMD_WRITE) {
             /* This is a write command containing 4 bytes of data that
             * should be written to the given page address. */
             uint8_t PageAddress = Buffer[1];
@@ -185,8 +191,7 @@ else
                 Buffer[0] = NAK_CRC_ERROR;
                 return NAK_FRAME_SIZE;
             }
-        }
-     else if (Buffer[0] == CMD_COMPAT_WRITE) {
+        } else if (Cmd == CMD_COMPAT_WRITE) {
             /* The Mifare compatbility write command is a 2-frame command.
             * The first frame contains the page-address and the second frame
             * holds the data. */
@@ -210,9 +215,55 @@ else
                 Buffer[0] = NAK_CRC_ERROR;
                 return NAK_FRAME_SIZE;
             }
+        } else if (Cmd == CMD_HALT) {
+            /* Halts the tag. According to the ISO14443, the second
+            * byte is supposed to be 0. */
+            if (Buffer[1] == 0) {
+                if (ISO14443ACheckCRCA(Buffer, 2)) {
+                    /* According to ISO14443, we must not send anything
+                    * in order to acknowledge the HALT command. */
+                    State = STATE_HALT;
+                    return ISO14443A_APP_NO_RESPONSE;
+                } else {
+                    Buffer[0] = NAK_CRC_ERROR;
+                    return NAK_FRAME_SIZE;
+                }
+            } else {
+                Buffer[0] = NAK_INVALID_ARG;
+                return NAK_FRAME_SIZE;
+            }
+        } else {
+            /* Unknown command. Enter halt state */
+            State = STATE_IDLE;
+        }
+    break;
+
+    case STATE_COMPAT_WRITE:
+        /* Compatibility write. Receiving 16 bytes of data of which 4 bytes are valid. */
+        if (ISO14443ACheckCRCA(Buffer, BYTES_PER_COMPAT_WRITE)) {
+            /* We don't perform any checks here. You will be able to program the
+            * whole memory. Also there is no OTP behaviour. */
+            if (!ActiveConfiguration.ReadOnly) {
+                MemoryWriteBlock(Buffer, CompatWritePageAddress * BYTES_PER_PAGE, BYTES_PER_WRITE);
+            } else {
+                /* If we are told to be read only, we silently ignore the write command
+                * and pretend to have written data. */
+            }
+
+            State = STATE_ACTIVE;
+            Buffer[0] = ACK_VALUE;
+            return ACK_FRAME_SIZE;
+        } else {
+            State = STATE_ACTIVE;
+            Buffer[0] = NAK_CRC_ERROR;
+            return NAK_FRAME_SIZE;
         }
 
-}
+    default:
+        /* Unknown state? Should never happen. */
+        break;
+    }
+
     /* No response has been sent, when we reach here */
     return ISO14443A_APP_NO_RESPONSE;
 }
