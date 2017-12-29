7125c95288b6c0c9d24e9c7292057b80d98981bb
bogiton <34060135+bogiton@users.noreply.github.com>
2017-12-29 23:56:35 +0000

    Single button support

--- Firmware/Chameleon-Mini/Button.h
+++ Firmware/Chameleon-Mini/Button.h
@@ -15,41 +15,26 @@
 #define BUTTON_PORT     PORTA
 #define BUTTON_MASK     PIN6_bm
 #define BUTTON_PINCTRL	PIN6CTRL
-															
 
 typedef enum {
-    BUTTON_R_PRESS_SHORT = 0,
-    BUTTON_R_PRESS_LONG,
-    BUTTON_L_PRESS_SHORT,
-    BUTTON_L_PRESS_LONG,
+	BUTTON_ACTION_NONE,
+	BUTTON_ACTION_UID_RANDOM,
+	BUTTON_ACTION_UID_LEFT_INCREMENT,
+	BUTTON_ACTION_UID_RIGHT_INCREMENT,
+	BUTTON_ACTION_UID_LEFT_DECREMENT,
+	BUTTON_ACTION_UID_RIGHT_DECREMENT,
+	BUTTON_ACTION_CYCLE_SETTINGS,
 
-    /* Must be last element */
-    BUTTON_TYPE_COUNT
-} ButtonTypeEnum;
-
-typedef enum {
-    BUTTON_ACTION_NONE,
-    BUTTON_ACTION_UID_RANDOM,
-    BUTTON_ACTION_UID_LEFT_INCREMENT,
-    BUTTON_ACTION_UID_RIGHT_INCREMENT,
-    BUTTON_ACTION_UID_LEFT_DECREMENT,
-    BUTTON_ACTION_UID_RIGHT_DECREMENT,
-    BUTTON_ACTION_CYCLE_SETTINGS,
-    BUTTON_ACTION_STORE_MEM,
-    BUTTON_ACTION_RECALL_MEM,
-    BUTTON_ACTION_TOGGLE_FIELD,
-    BUTTON_ACTION_STORE_LOG,
-
-    /* This has to be last element */
-    BUTTON_ACTION_COUNT
+	/* This has to be last element */
+	BUTTON_ACTION_COUNT
 } ButtonActionEnum;
 
 void ButtonInit(void);
 void ButtonTick(void);
 
 void ButtonGetActionList(char* ListOut, uint16_t BufferSize);
-void ButtonSetActionById(ButtonTypeEnum Type, ButtonActionEnum Action);
-void ButtonGetActionByName(ButtonTypeEnum Type, char* ActionOut, uint16_t BufferSize);
-bool ButtonSetActionByName(ButtonTypeEnum Type, const char* Action);
+void ButtonSetActionById(ButtonActionEnum Action);
+void ButtonGetActionByName(char* ActionOut, uint16_t BufferSize);
+bool ButtonSetActionByName(const char* Action);
 
 #endif /* BUTTON_H_ */
