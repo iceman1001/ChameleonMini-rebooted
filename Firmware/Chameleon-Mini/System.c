06d6f30e7e3982841214267b0839370229d815ba
bogiton <34060135+bogiton@users.noreply.github.com>
2017-12-30 00:10:57 +0000

    Update System.c

--- Firmware/Chameleon-Mini/System.c
+++ Firmware/Chameleon-Mini/System.c
@@ -1,91 +1,91 @@
-#include "Settings.h"
-#include <avr/eeprom.h>
-#include "Configuration.h"
-#include <string.h>
-#include "LED.h"
-#include "Log.h"
-#include "Memory.h"
-#include "Terminal/CommandLine.h"
+
 #include "System.h"
+#include "LED.h"
 
-#define SETTING_TO_INDEX(S) (S - SETTINGS_FIRST)
-#define INDEX_TO_SETTING(I) (I + SETTINGS_FIRST)														
-
-SettingsType GlobalSettings = {
-	.ActiveSetting = DEFAULT_SETTING,
-	.ActiveSettingPtr = &GlobalSettings.Settings[DEFAULT_SETTING],
-	.Settings = { [0 ... (SETTINGS_COUNT-1)] =	{
-				.Configuration = DEFAULT_CONFIGURATION,
-				.ButtonAction = DEFAULT_BUTTON_ACTION,
-				} 
-			}
-};
-SettingsType EEMEM StoredSettings = {
-	.ActiveSetting = DEFAULT_SETTING,
-	.ActiveSettingPtr = &GlobalSettings.Settings[DEFAULT_SETTING],
-
-	.Settings = { [0 ... (SETTINGS_COUNT-1)] =	{
-			.Configuration = DEFAULT_CONFIGURATION,
-			.ButtonAction = DEFAULT_BUTTON_ACTION,
-			}
-		}
-};
-
-void SettingsLoad(void) {
-	eeprom_read_block(&GlobalSettings, &StoredSettings, sizeof(SettingsType));
+ISR(BADISR_vect)
+{
+    while(1);
 }
 
-void SettingsSave(void) {
-#if ENABLE_EEPROM_SETTINGS
-	eeprom_write_block(&GlobalSettings, &StoredSettings, sizeof(SettingsType));
-#endif
+void SystemInit(void)
+{
+    if (RST.STATUS & RST_WDRF_bm) {
+        /* On Watchdog reset clear WDRF bit, disable watchdog
+        * and jump into bootloader */
+        RST.STATUS = RST_WDRF_bm;
+
+        CCP = CCP_IOREG_gc;
+        WDT.CTRL = WDT_CEN_bm;
+
+        asm volatile ("jmp %0"::"i" (BOOT_SECTION_START + 0x1FC));
+    }
+
+    /* 32MHz system clock using internal RC and 32K DFLL*/
+    OSC.CTRL |= OSC_RC32MEN_bm | OSC_RC32KEN_bm;
+    while(!(OSC.STATUS & OSC_RC32MRDY_bm))
+        ;
+    while(!(OSC.STATUS & OSC_RC32KRDY_bm))
+        ;
+
+    OSC.DFLLCTRL = OSC_RC32MCREF_RC32K_gc;
+    DFLLRC32M.CTRL = DFLL_ENABLE_bm;
+
+    CCP = CCP_IOREG_gc;
+    CLK.CTRL = CLK_SCLKSEL_RC32M_gc;
+
+    /* Use TCE0 as system tick */
+    TCE0.PER = F_CPU / 256 / SYSTEM_TICK_FREQ - 1;
+    TCE0.CTRLA = TC_CLKSEL_DIV256_gc;
+
+    /* Enable RTC with roughly 1kHz clock */
+    CLK.RTCCTRL = CLK_RTCSRC_ULP_gc | CLK_RTCEN_bm;
+    RTC.CTRL = RTC_PRESCALER_DIV1_gc;
+
+    /* Enable EEPROM data memory mapping */
+    NVM.CTRLB |= NVM_EEMAPEN_bm;
 }
 
-void SettingsCycle(void) {
-	char i = SETTINGS_COUNT;
-	uint8_t Setting = GlobalSettings.ActiveSetting;
+void SystemReset(void)
+{
+    CCP = CCP_IOREG_gc;
+    RST.CTRL = RST_SWRST_bm;
 
-	while (i-- > 0) {
-		Setting = (Setting + 1) % SETTINGS_COUNT;
-
-		if (GlobalSettings.Settings[Setting].Configuration != CONFIG_NONE) {
-			SettingsSetActiveById(Setting);
-			break;
-		}
-	}
 }
 
-bool SettingsSetActiveById(uint8_t Setting) {
-	if (Setting < SETTINGS_COUNT) {
-		GlobalSettings.ActiveSetting = Setting;
-		GlobalSettings.ActiveSettingPtr = &GlobalSettings.Settings[GlobalSettings.ActiveSetting];
-
-		/* Settings have changed. Progress changes through system */
-		ConfigurationInit();
-		return true;
-	} else {
-		return false;
-	}
+void SystemEnterBootloader(void)
+{
+    /* Use Watchdog timer to reset into bootloader. */
+    CCP = CCP_IOREG_gc;
+    WDT.CTRL = WDT_PER_512CLK_gc | WDT_ENABLE_bm | WDT_CEN_bm;
 }
 
-uint8_t SettingsGetActiveById(void) {
-	return GlobalSettings.ActiveSetting;
+
+void SystemStartUSBClock(void)
+{
+    /* 48MHz USB Clock using 12MHz XTAL */
+    OSC.XOSCCTRL = OSC_FRQRANGE_12TO16_gc | OSC_XOSCSEL_XTAL_16KCLK_gc;
+    OSC.CTRL |= OSC_XOSCEN_bm;
+    while(!(OSC.STATUS & OSC_XOSCRDY_bm))
+        ;
+
+    OSC.PLLCTRL = OSC_PLLSRC_XOSC_gc | (4 << OSC_PLLFAC_gp);
+
+    OSC.CTRL |= OSC_PLLEN_bm;
+    while(!(OSC.STATUS & OSC_PLLRDY_bm))
+        ;
 }
 
-void SettingsGetActiveByName(char* SettingOut, uint16_t BufferSize) {
-	SettingOut[0] = 'N';
-	SettingOut[1] = 'O';
-	SettingOut[2] = '.';	
-	SettingOut[3] = SettingsGetActiveById() + '0';
-	SettingOut[4] = '\0';
+void SystemStopUSBClock(void)
+{
+    /* Disable USB Clock to minimize power consumption */
+    CLK.USBCTRL &= ~CLK_USBSEN_bm;
+    OSC.CTRL &= ~OSC_PLLEN_bm;
+    OSC.CTRL &= ~OSC_XOSCEN_bm;
 }
 
-bool SettingsSetActiveByName(const char* Setting) {
-	uint8_t SettingNr = Setting[0] - '0';
-
-	if ((Setting[1] == '\0') && (SettingNr < SETTINGS_COUNT)) {
-		return SettingsSetActiveById(SettingNr);
-	} else {
-		return false;
-	}
+void SystemInterruptInit(void)
+{
+    /* Enable all interrupt levels */
+    PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
+    sei();
 }
