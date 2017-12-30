04669748c2525f8f9d92f6eb88bc9621d436385c
bogiton <34060135+bogiton@users.noreply.github.com>
2017-12-30 00:12:37 +0000

    Removed LEDHook

--- Firmware/Chameleon-Mini/Settings.h
+++ Firmware/Chameleon-Mini/Settings.h
@@ -23,11 +23,9 @@
  * \note Some properties may change globally if this is defined in the Makefile.
  */
 typedef struct {
-    ButtonActionEnum ButtonActions[BUTTON_TYPE_COUNT]; /// Button actions for this setting.
+    ButtonActionEnum ButtonAction; /// Button action for this setting.
     LogModeEnum LogMode; /// Log mode for this setting.
     ConfigurationEnum Configuration; /// Active configuration for this setting.
-    LEDHookEnum LEDRedFunction; /// Red LED function for this setting.
-    LEDHookEnum LEDGreenFunction; /// Green LED function for this setting.
     uint16_t PendingTaskTimeout; /// Timeout for timeout commands for this setting, in multiples of 100 ms.
 } SettingsEntryType;
 
