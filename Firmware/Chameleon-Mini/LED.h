5cc175e9e577a8b909edc998519df0cdfee64146
bogiton <34060135+bogiton@users.noreply.github.com>
2019-03-05 13:24:45 +0000

    Update LED.h

--- Firmware/Chameleon-Mini/LED.h
+++ Firmware/Chameleon-Mini/LED.h
@@ -9,14 +9,15 @@
 #define LED_H
 
 #include "Common.h"
+#include "Settings.h"
 #include <avr/io.h>
 
-#define LED_HEIGH_PORT  PORTA
+#define LED_HIGH_PORT  PORTA
 #define LED_ONE	        PIN5_bm
 #define LED_TWO		    PIN4_bm
 #define LED_THREE		PIN3_bm
 #define LED_FOUR		PIN2_bm
-#define LED_HEIGH_MASK	(LED_ONE | LED_TWO | LED_THREE| LED_FOUR)
+#define LED_HIGH_MASK	(LED_ONE | LED_TWO | LED_THREE| LED_FOUR)
 
 #define LED_LOW_PORT    PORTE
 #define LED_FIVE	    PIN3_bm
@@ -25,88 +26,12 @@
 #define LED_EIGHT		PIN0_bm
 #define LED_LOW_MASK	(LED_FIVE | LED_SIX | LED_SEVEN| LED_EIGHT)
 			
-extern uint8_t LEDHighPulseMask, LEDLowPulseMask;
-
-static inline
-void LEDInit(void) {
-	LED_LOW_PORT.DIRSET = LED_LOW_MASK;
-	LED_HEIGH_PORT.DIRSET = LED_HEIGH_MASK;
-}
-
-static inline
-void LEDLowSetOn(uint8_t Mask) {
-	LED_HEIGH_PORT.OUTSET = Mask;
-}
-
-static inline
-void LEDLowSetOff(uint8_t Mask) {
-	LED_HEIGH_PORT.OUTCLR = Mask;
-}
-
-static inline
-void LEDHighSetOn(uint8_t Mask) {
-	LED_LOW_PORT.OUTSET = Mask;
-}
-
-static inline
-void LEDHighSetOff(uint8_t Mask) {
-	LED_LOW_PORT.OUTCLR = Mask;
-}
-
-static inline
-void LEDLowToggle(uint8_t Mask) {
-	LED_HEIGH_PORT.OUTTGL = Mask;
-}
-
-static inline
-void LEDHighToggle(uint8_t Mask) {
-	LED_LOW_PORT.OUTTGL = Mask;
-}
-
-static inline
-void LEDLowPulse(uint8_t Mask) {
-	LEDLowPulseMask = Mask;
-	LED_HEIGH_PORT.OUTSET = Mask;
-}
-
-static inline
-void LEDHighPulse(uint8_t Mask) {
-	LEDHighPulseMask = Mask;
-	LED_LOW_PORT.OUTSET = Mask;
-}
-
-static inline
-void LEDTick(void) {
-	LED_HEIGH_PORT.OUTCLR = LEDLowPulseMask;
-	LEDLowPulseMask = 0;
-	LED_LOW_PORT.OUTCLR = LEDHighPulseMask;
-	LEDHighPulseMask = 0;
-}
-
-static inline
-void CARDInit(void) {
-	LED_HEIGH_PORT.DIRSET = LED_HEIGH_MASK;
-	LED_LOW_PORT.DIRSET = LED_LOW_MASK;
-}
-
-static inline
-void CARDHEIGHSetOn(uint8_t Mask) {
-	LED_HEIGH_PORT.OUTSET = Mask;
-}
-
-static inline
-void CARDLOWSetOn(uint8_t Mask) {
-	LED_LOW_PORT.OUTSET = Mask;
-}
-
-static inline
-void CARDHEIGHSetOff(uint8_t Mask) {
-	LED_HEIGH_PORT.OUTCLR = Mask;
-}
-
-static inline
-void CARDLOWSetOff(uint8_t Mask) {
-	LED_LOW_PORT.OUTCLR = Mask;
-}
+void LEDInit(void);
+void LEDTick(void);
+void CARDInit(void);
+void LEDHighSetOn(uint8_t Mask);
+void LEDLowSetOn(uint8_t Mask);
+void LEDHighSetOff(uint8_t Mask);
+void LEDLowSetOff(uint8_t Mask);
 
 #endif /* LED_H */
