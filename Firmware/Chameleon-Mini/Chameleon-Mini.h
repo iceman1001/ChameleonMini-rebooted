9f755b891d8eb9d4a41b1b157786e12a5e71b336
bogiton <34060135+bogiton@users.noreply.github.com>
2019-03-05 13:26:14 +0000

    Update Chameleon-Mini.h

--- Firmware/Chameleon-Mini/Chameleon-Mini.h
+++ Firmware/Chameleon-Mini/Chameleon-Mini.h
@@ -6,8 +6,6 @@
 #include <avr/interrupt.h>
 #include <avr/power.h>
 
-
-
 #include "System.h"
 #include "Memory.h"
 #include "LED.h"
@@ -21,7 +19,5 @@
 #include "AntennaLevel.h"
 #include "Settings.h"
 #include "sp_driver.h"
-#define  CHAMELEON_MINI_VERSION_STRING    "Old_Driver_2016"
 
 #endif //CHAMELEON_MINI_H
-
