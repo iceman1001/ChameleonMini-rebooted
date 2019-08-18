2b0f2c7a60670937e93c159fa8f6a60bffae2ca2
@shinhub <shinhub@users.noreply.github.com>
2019-08-18 15:13:49 +0200

    Unifying names and ensuring backwards compatibility in our own copy of Windows flashing tools

--- Software/Flashing-Windows/flash.bat
+++ Software/Flashing-Windows/flash.bat
@@ -3,66 +3,80 @@
 echo.                
 echo .---------------------------------------------------.
 echo ^|                                                   ^|
-echo ^|   Chameleon-Mini Rev-E Rebooted flasher utility   ^|
+echo ^|   ChameleonMini RevE Rebooted flasher utility     ^|
+echo ^|                  Iceman fork                      ^|
 echo ^|                                                   ^|
 echo `---------------------------------------------------'
 echo.
 
-if not exist "%~dp0BOOT_LOADER_EXE.exe" (
-	echo Cannot find BOOT_LOADER_EXE.exe. Please run this .bat script in the same directory where BOOT_LOADER_EXE.exe is saved.
+set dirpath=%~dp0
+set oldname=Chameleon-Mini
+set name=ChameleonMini
+
+if not exist "%dirpath%BOOT_LOADER_EXE.exe" (
+	echo Cannot find BOOT_LOADER_EXE.exe. Please run this .bat script in the same directory where it is saved.
 	pause > nul
 	exit
 )
 
-if not exist "%~dp0avr-objcopy.exe" (
-	echo Cannot find avr-objcopy.exe. Please run this .bat script in the same directory where avr-objcopy.exe is saved.
+if not exist "%dirpath%avr-objcopy.exe" (
+	echo Cannot find avr-objcopy.exe. Please run this .bat script in the same directory where it is saved.
 	pause > nul
 	exit
 )
 
-if not exist "%~dp0Createbin.exe" (
-	echo Cannot find Createbin.exe. Please run this .bat script in the same directory where Createbin.exe is saved.
+if not exist "%dirpath%Createbin.exe" (
+	echo Cannot find Createbin.exe. Please run this .bat script in the same directory where it is saved.
 	pause > nul
 	exit
 )
 
-if not exist "%~dp0Chameleon-Mini.eep" (
-	echo Cannot find Chameleon-Mini.eep. Please run this .bat script in the same directory where Chameleon-Mini.eep and Chameleon-Mini.hex are saved.
-	pause > nul
-	exit
+if not exist "%dirpath%%name%.eep" (
+	if exist "%dirpath%%oldname%.eep" (
+		move /y %dirpath%%oldname%.eep %dirpath%%name%.eep
+	) else (
+		echo Cannot find eep file. Please run this .bat script in the same directory where it is saved.
+		pause > nul
+		exit
+	)
 )
 
-if not exist "%~dp0Chameleon-Mini.hex" (
-	echo Cannot find Chameleon-Mini.hex. Please run this .bat script in the same directory where Chameleon-Mini.eep and Chameleon-Mini.hex are saved.
-	pause > nul
-	exit
+if not exist "%dirpath%%name%.hex" (
+	if exist "%dirpath%%oldname%.hex" (
+		move /y %dirpath%%oldname%.hex %dirpath%%name%.hex
+	) else (
+		echo Cannot find hex file. Please run this .bat script in the same directory where it is saved.
+		pause > nul
+		exit
+	)
 )
 
 echo Creating the EEPROM binary...
-"%~dp0avr-objcopy.exe" -I ihex "%~dp0Chameleon-Mini.eep" -O binary eeprom.bin
+"%dirpath%avr-objcopy.exe" -I ihex %name%.eep -O binary eeprom.bin
 
-"%~dp0Createbin.exe" eeprom.bin bin
+"%dirpath%Createbin.exe" eeprom.bin bin
 
-del eeprom.bin
+del %dirpath%eeprom.bin
 
 echo.
 echo.
 
-move myfile.bin myfilee.bin >nul
+move %dirpath%myfile.bin %dirpath%myfilee.bin >nul
 
 echo Creating the Flash binary...
-"%~dp0avr-objcopy.exe" -I ihex "%~dp0Chameleon-Mini.hex" -O binary flash.bin
+"%dirpath%avr-objcopy.exe" -I ihex %name%.hex -O binary flash.bin
 
-"%~dp0Createbin.exe" flash.bin bin
+"%dirpath%Createbin.exe" flash.bin bin
 
-del flash.bin
-
-echo.
-echo.
-
-echo Flashing the files onto the "Chameleon-Mini Rev-E Rebooted"...
-"%~dp0BOOT_LOADER_EXE.exe"
+del %dirpath%flash.bin
 
 echo.
+echo.
 
-echo If there are no errors above, flashing the firmware to your "Chameleon-Mini Rev-E Rebooted" should be finished now. Enjoy!
+echo Flashing the files onto the "ChameleonMini RevE Rebooted"...
+"%dirpath%BOOT_LOADER_EXE.exe"
+
+echo.
+
+echo If there are no errors above, flashing the firmware should be finished now. Enjoy!
+
