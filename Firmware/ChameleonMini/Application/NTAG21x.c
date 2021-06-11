9b2e2b0461494cea475d59e74e7d0ce7dfba17b5
Noe Xu <xeonxu@gmail.com>
2021-06-11 09:11:43 +0800

    Fix ultralight compile issue.

--- Firmware/ChameleonMini/Application/NTAG21x.c
+++ Firmware/ChameleonMini/Application/NTAG21x.c
@@ -9,12 +9,86 @@
  *  Thanks to skuser for the MifareUltralight code used as a starting point
  */
 
-#if defined (CONFIG_NTAG213_SUPPORT) || defined (CONFIG_NTAG215_SUPPORT) || defined (CONFIG_NTAG216_SUPPORT)
-
 #include "NTAG21x.h"
 #include "ISO14443-3A.h"
 #include "../Codec/ISO14443-2A.h"
 #include "../Memory/Memory.h"
+
+//DEFINE ATQA and SAK
+#define ATQA_VALUE 0x0044
+#define SAK_VALUE 0x00
+
+#define SAK_CL1_VALUE           ISO14443A_SAK_INCOMPLETE
+#define SAK_CL2_VALUE           ISO14443A_SAK_COMPLETE_NOT_COMPLIANT
+
+//ACK and NACK
+#define ACK_VALUE                   0x0A
+#define ACK_FRAME_SIZE          4 /* Bits */
+#define NAK_INVALID_ARG             0x00
+#define NAK_CRC_ERROR               0x01
+#define NAK_NOT_AUTHED              0x04
+#define NAK_EEPROM_ERROR            0x05
+#define NAK_FRAME_SIZE          4
+
+//DEFINING COMMANDS
+/* ISO commands */
+#define CMD_HALT 0x50
+//NTAG COMMANDS
+#define CMD_GET_VERSION 0x60
+#define CMD_READ 0x30
+#define CMD_FAST_READ 0x3A
+#define CMD_WRITE 0xA2
+#define CMD_COMPAT_WRITE 0xA0
+#define CMD_READ_CNT 0x39
+#define CMD_PWD_AUTH 0x1B
+#define CMD_READ_SIG 0x3C
+
+
+//MEMORY LAYOUT STUFF, addresses and sizes in bytes
+//UID stuff
+#define UID_CL1_ADDRESS         0x00
+#define UID_CL1_SIZE            3
+#define UID_BCC1_ADDRESS        0x03
+#define UID_CL2_ADDRESS         0x04
+#define UID_CL2_SIZE            4
+#define UID_BCC2_ADDRESS        0x08
+//LockBytes stuff
+#define STATIC_LOCKBYTE_0_ADDRESS   0x0A
+#define STATIC_LOCKBYTE_1_ADDRESS   0x0B
+//CONFIG stuff
+#define NTAG213_CONFIG_AREA_START_ADDRESS   NTAG21x_PAGE_SIZE * 0x29
+#define NTAG215_CONFIG_AREA_START_ADDRESS   NTAG21x_PAGE_SIZE * 0x83
+#define NTAG216_CONFIG_AREA_START_ADDRESS   NTAG21x_PAGE_SIZE * 0xE3
+#define CONFIG_AREA_SIZE        8
+//CONFIG offsets, relative to config start address
+#define CONF_AUTH0_OFFSET       0x03
+#define CONF_ACCESS_OFFSET      0x04
+#define CONF_PASSWORD_OFFSET    0x08
+#define CONF_PACK_OFFSET        0x0C
+
+//WRITE STUFF
+#define BYTES_PER_WRITE         4
+#define PAGE_WRITE_MIN          0x02
+
+//CONFIG masks to check individual needed bits
+#define CONF_ACCESS_PROT        0x80
+
+#define VERSION_INFO_LENGTH 8 //8 bytes info lenght + crc
+
+#define BYTES_PER_READ NTAG21x_PAGE_SIZE * 4
+//SIGNATURE Lenght
+#define SIGNATURE_LENGTH        32
+
+/* #define NTAG21x_UID_SIZE ISO14443A_UID_SIZE_DOUBLE //7 bytes UID */
+
+/* #define NTAG21x_PAGE_SIZE 4 //bytes per page */
+/* #define NTAG213_PAGES 45 //45 pages total for ntag213, from 0 to 44 */
+/* #define NTAG215_PAGES 135 //135 pages total for ntag215, from 0 to 134 */
+/* #define NTAG216_PAGES 231 //231 pages total for ntag216, from 0 to 230 */
+
+/* #define NTAG213_MEM_SIZE ( NTAG21x_PAGE_SIZE * NTAG213_PAGES ) */
+/* #define NTAG215_MEM_SIZE ( NTAG21x_PAGE_SIZE * NTAG215_PAGES ) */
+/* #define NTAG216_MEM_SIZE ( NTAG21x_PAGE_SIZE * NTAG216_PAGES ) */
 
 static enum {
     TYPE_NTAG213,
@@ -42,7 +116,7 @@ static uint8_t Access;
 
 
 //Writes a page
-uint8_t AppWritePage(uint8_t PageAddress, uint8_t *const Buffer) {
+static uint8_t AppWritePage(uint8_t PageAddress, uint8_t *const Buffer) {
     if (!ActiveConfiguration.ReadOnly) {
         AppCardMemoryWrite(Buffer, PageAddress * NTAG21x_PAGE_SIZE, NTAG21x_PAGE_SIZE);
     } else {
@@ -104,12 +178,18 @@ void NTAG21xAppReset(void) {
     State = STATE_IDLE;
 }
 
+#if defined(CONFIG_NTAG213_SUPPORT)
+
 void NTAG213AppInit(void) {
     Ntag_Type = TYPE_NTAG213;
     PageCount = NTAG213_PAGES;
     ConfigStartAddr = NTAG213_CONFIG_AREA_START_ADDRESS;
     NTAG21xAppInit();
 }
+
+#endif // CONIFG_NTAG213_SUPPORT
+
+#ifdef CONFIG_NTAG215_SUPPORT
 
 void NTAG215AppInit(void) {
     Ntag_Type = TYPE_NTAG215;
@@ -118,12 +198,18 @@ void NTAG215AppInit(void) {
     NTAG21xAppInit();
 }
 
+#endif // CONFIG_NTAG215_SUPPORT
+
+#ifdef CONFIG_NTAG216_SUPPORT
+
 void NTAG216AppInit(void) {
     Ntag_Type = TYPE_NTAG216;
     PageCount = NTAG216_PAGES;
     ConfigStartAddr = NTAG216_CONFIG_AREA_START_ADDRESS;
     NTAG21xAppInit();
 }
+
+#endif // CONFIG_NTAG216_SUPPORT
 
 //Verify authentication
 static bool VerifyAuthentication(uint8_t PageAddress) {
@@ -418,5 +504,3 @@ uint16_t NTAG21xAppProcess(uint8_t *Buffer, uint16_t BitCount) {
     /* No response has been sent, when we reach here */
     return ISO14443A_APP_NO_RESPONSE;
 }
-
-#endif
--- Firmware/ChameleonMini/Application/NTAG21x.h
+++ Firmware/ChameleonMini/Application/NTAG21x.h
@@ -10,71 +10,6 @@
 #include "Application.h"
 #include "ISO14443-3A.h"
 
-//DEFINE ATQA and SAK
-#define ATQA_VALUE 0x0044
-#define SAK_VALUE 0x00
-
-#define SAK_CL1_VALUE           ISO14443A_SAK_INCOMPLETE
-#define SAK_CL2_VALUE           ISO14443A_SAK_COMPLETE_NOT_COMPLIANT
-
-//ACK and NACK
-#define ACK_VALUE                   0x0A
-#define ACK_FRAME_SIZE          4 /* Bits */
-#define NAK_INVALID_ARG             0x00
-#define NAK_CRC_ERROR               0x01
-#define NAK_NOT_AUTHED              0x04
-#define NAK_EEPROM_ERROR            0x05
-#define NAK_FRAME_SIZE          4
-
-//DEFINING COMMANDS
-/* ISO commands */
-#define CMD_HALT 0x50
-//NTAG COMMANDS
-#define CMD_GET_VERSION 0x60
-#define CMD_READ 0x30
-#define CMD_FAST_READ 0x3A
-#define CMD_WRITE 0xA2
-#define CMD_COMPAT_WRITE 0xA0
-#define CMD_READ_CNT 0x39
-#define CMD_PWD_AUTH 0x1B
-#define CMD_READ_SIG 0x3C
-
-
-//MEMORY LAYOUT STUFF, addresses and sizes in bytes
-//UID stuff
-#define UID_CL1_ADDRESS         0x00
-#define UID_CL1_SIZE            3
-#define UID_BCC1_ADDRESS        0x03
-#define UID_CL2_ADDRESS         0x04
-#define UID_CL2_SIZE            4
-#define UID_BCC2_ADDRESS        0x08
-//LockBytes stuff
-#define STATIC_LOCKBYTE_0_ADDRESS   0x0A
-#define STATIC_LOCKBYTE_1_ADDRESS   0x0B
-//CONFIG stuff
-#define NTAG213_CONFIG_AREA_START_ADDRESS   NTAG21x_PAGE_SIZE * 0x29
-#define NTAG215_CONFIG_AREA_START_ADDRESS   NTAG21x_PAGE_SIZE * 0x83
-#define NTAG216_CONFIG_AREA_START_ADDRESS   NTAG21x_PAGE_SIZE * 0xE3
-#define CONFIG_AREA_SIZE        8
-//CONFIG offsets, relative to config start address
-#define CONF_AUTH0_OFFSET       0x03
-#define CONF_ACCESS_OFFSET      0x04
-#define CONF_PASSWORD_OFFSET    0x08
-#define CONF_PACK_OFFSET        0x0C
-
-//WRITE STUFF
-#define BYTES_PER_WRITE         4
-#define PAGE_WRITE_MIN          0x02
-
-//CONFIG masks to check individual needed bits
-#define CONF_ACCESS_PROT        0x80
-
-#define VERSION_INFO_LENGTH 8 //8 bytes info lenght + crc
-
-#define BYTES_PER_READ NTAG21x_PAGE_SIZE * 4
-//SIGNATURE Lenght
-#define SIGNATURE_LENGTH        32
-
 #define NTAG21x_UID_SIZE ISO14443A_UID_SIZE_DOUBLE //7 bytes UID
 
 #define NTAG21x_PAGE_SIZE 4 //bytes per page
@@ -103,7 +38,5 @@ uint16_t NTAG21xAppProcess(uint8_t *Buffer, uint16_t BitCount);
 
 void NTAG21xGetUid(ConfigurationUidType Uid);
 void NTAG21xSetUid(ConfigurationUidType Uid);
-
-uint8_t AppWritePage(uint8_t PageAddress, uint8_t *const Buffer);
 
 #endif
